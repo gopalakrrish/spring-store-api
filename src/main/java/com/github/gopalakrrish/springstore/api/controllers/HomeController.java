2701cfe9828f151ebe5e5f26e966950abdc518ef
Gopala Krishna <gopalakrrish15@gmail.com>
2026-01-25 16:00:33 +0530

    Switch home page from static to template-based rendering

--- src/main/java/com/github/gopalakrrish/springstore/api/controllers/HomeController.java
+++ src/main/java/com/github/gopalakrrish/springstore/api/controllers/HomeController.java
@@ -1,17 +1,15 @@
 package com.github.gopalakrrish.springstore.api.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index(){
-        return "index.html";
+    public String index(Model model){
+        model.addAttribute("name", "gk");
+        return "index";
     }
 
-    @RequestMapping("/hello")
-    public String sayHello(){
-        return "index.html";
-    }
 }
