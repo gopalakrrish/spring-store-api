1a7032902194c69b07b35e108d37a01d89c7e34d
Gopala Krishna <gopalakrrish15@gmail.com>
2026-01-28 18:44:34 +0530

    Add sorting support to users listing endpoint

--- src/main/java/com/github/gopalakrrish/springstore/api/controllers/UserController.java
+++ src/main/java/com/github/gopalakrrish/springstore/api/controllers/UserController.java
@@ -5,13 +5,12 @@ import com.github.gopalakrrish.springstore.api.entities.User;
 import com.github.gopalakrrish.springstore.api.mappers.UserMapper;
 import com.github.gopalakrrish.springstore.api.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -22,8 +21,13 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping()
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUsers(
+            @RequestParam(required = false, defaultValue = "", name = "sort")
+            String sortBy) {
+        if (!Set.of("name","email").contains(sortBy))
+            sortBy = "name";
+
+        return userRepository.findAll(Sort.by(sortBy))
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
