2923c8d97413eb00b7e3e004ee8f8bbffd04ad73
Gopala Krishna <gopalakrrish15@gmail.com>
2026-01-29 12:00:26 +0530

    Add endpoint to create user

--- src/main/java/com/github/gopalakrrish/springstore/api/controllers/UserController.java
+++ src/main/java/com/github/gopalakrrish/springstore/api/controllers/UserController.java
@@ -22,10 +22,8 @@ public class UserController {
 
     @GetMapping()
     public Iterable<UserDto> getAllUsers(
-            @RequestHeader(required = false, name = "x-auth-token") String authToken,
             @RequestParam(required = false, defaultValue = "", name = "sort")
             String sortBy) {
-        System.out.println(authToken);
 
         if (!Set.of("name","email").contains(sortBy))
             sortBy = "name";
@@ -43,6 +41,11 @@ public class UserController {
             return ResponseEntity.notFound().build();
 
         return ResponseEntity.ok(userMapper.toDto(user));
+    }
+
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto data) {
+        return data;
     }
 
 }
