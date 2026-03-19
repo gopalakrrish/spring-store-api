a0809e0617048b97f397e3a1662a5b99f5eebd63
Gopala Krishna <gopalakrrish15@gmail.com>
2026-03-19 06:35:08 +0530

    Configure endpoint authorization with public and secured routes

--- src/main/java/com/github/gopalakrrish/springstore/api/config/SecurityConfig.java
+++ src/main/java/com/github/gopalakrrish/springstore/api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.github.gopalakrrish.springstore.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,7 +17,11 @@ public class SecurityConfig {
         http
             .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(c -> c.anyRequest().permitAll());
+            .authorizeHttpRequests(c -> c
+                    .requestMatchers("/carts/**").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/users").permitAll()
+                    .anyRequest().authenticated()
+            );
 
         return http.build();
     }
