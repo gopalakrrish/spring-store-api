74821e0ff76fdcff38e6229ae88f4741f6a09efd
Gopala Krishna <gopalakrrish15@gmail.com>
2026-03-19 15:09:51 +0530

    Configure BCrypt password encoder

--- src/main/java/com/github/gopalakrrish/springstore/api/config/SecurityConfig.java
+++ src/main/java/com/github/gopalakrrish/springstore/api/config/SecurityConfig.java
@@ -7,11 +7,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -19,7 +26,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(c -> c
                     .requestMatchers("/carts/**").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/users").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/users").permitAll()
                     .anyRequest().authenticated()
             );
 
