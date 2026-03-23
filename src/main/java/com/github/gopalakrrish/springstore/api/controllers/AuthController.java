c3866491f916b57d79b59a8f701e84cb06a46575
Gopala Krishna <gopalakrrish15@gmail.com>
2026-03-23 11:02:02 +0530

    Add JWT token generation and return token on login

--- src/main/java/com/github/gopalakrrish/springstore/api/controllers/AuthController.java
+++ src/main/java/com/github/gopalakrrish/springstore/api/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.github.gopalakrrish.springstore.api.controllers;
 
+import com.github.gopalakrrish.springstore.api.dtos.JwtResponse;
 import com.github.gopalakrrish.springstore.api.dtos.LoginRequest;
+import com.github.gopalakrrish.springstore.api.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,14 +17,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@Valid  @RequestBody LoginRequest request) {
+    public ResponseEntity<JwtResponse> login(@Valid  @RequestBody LoginRequest request)
+    {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 request.getEmail(), request.getPassword())
         );
 
-        return ResponseEntity.ok().build();
+        var token = jwtService.generateToken(request.getEmail());
+
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
