1117d8f14d81b1b1aeabd9a8e178e36c79cb62aa
Gopala Krishna <gopalakrrish15@gmail.com>
2026-05-27 12:18:07 +0530

    Extract order retrieval logic into OrderService

--- src/main/java/com/github/gopalakrrish/springstore/api/controllers/OrderController.java
+++ src/main/java/com/github/gopalakrrish/springstore/api/controllers/OrderController.java
@@ -1,9 +1,7 @@
 package com.github.gopalakrrish.springstore.api.controllers;
 
 import com.github.gopalakrrish.springstore.api.dtos.OrderDto;
-import com.github.gopalakrrish.springstore.api.mappers.OrderMapper;
-import com.github.gopalakrrish.springstore.api.repositories.OrderRepository;
-import com.github.gopalakrrish.springstore.api.services.AuthService;
+import com.github.gopalakrrish.springstore.api.services.OrderService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +13,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
-    private final AuthService authService;
-    private final OrderRepository orderRepository;
-    private final OrderMapper orderMapper;
+    private final OrderService orderService;
 
     @GetMapping
     public List<OrderDto> getAllOrders() {
-        var user = authService.getCurrentUser();
-        var orders = orderRepository.findAllByCustomer(user);
-        return  orders.stream().map(orderMapper::toDto).toList();
+        return orderService.getAllOrders();
     }
 
 }
---
+++ src/main/java/com/github/gopalakrrish/springstore/api/services/OrderService.java
@@ -0,0 +1,24 @@
+package com.github.gopalakrrish.springstore.api.services;
+
+import com.github.gopalakrrish.springstore.api.dtos.OrderDto;
+import com.github.gopalakrrish.springstore.api.mappers.OrderMapper;
+import com.github.gopalakrrish.springstore.api.repositories.OrderRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class OrderService {
+    private final AuthService authService;
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
+
+    public List<OrderDto> getAllOrders() {
+        var user = authService.getCurrentUser();
+        var orders = orderRepository.findAllByCustomer(user);
+        return  orders.stream().map(orderMapper::toDto).toList();
+    }
+
+}
