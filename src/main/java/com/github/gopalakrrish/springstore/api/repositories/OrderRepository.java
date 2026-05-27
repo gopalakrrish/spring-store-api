54a4a6237721b791fa3042af7e10f1e4d9cc3dea
Gopala Krishna <gopalakrrish15@gmail.com>
2026-05-27 12:50:35 +0530

    Fetch orders with items and products using EntityGraph to avoid N+1

--- src/main/java/com/github/gopalakrrish/springstore/api/repositories/OrderRepository.java
+++ src/main/java/com/github/gopalakrrish/springstore/api/repositories/OrderRepository.java
@@ -2,10 +2,15 @@ package com.github.gopalakrrish.springstore.api.repositories;
 
 import com.github.gopalakrrish.springstore.api.entities.Order;
 import com.github.gopalakrrish.springstore.api.entities.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findAllByCustomer(User customer);
+    @EntityGraph(attributePaths = "items.product")
+    @Query("SELECT o FROM Order o WHERE o.customer = :customer")
+    List<Order> getAllByCustomer(@Param("customer") User customer);
 }
