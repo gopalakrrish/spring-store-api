843571556a09e34b3f12575ee0a72fcb74fa51d9
Gopala Krishna <gopalakrrish15@gmail.com>
2026-06-03 12:36:03 +0530

    Handle Stripe exceptions with HTTP 500 response

--- src/main/java/com/github/gopalakrrish/springstore/api/controllers/CheckoutController.java
+++ src/main/java/com/github/gopalakrrish/springstore/api/controllers/CheckoutController.java
@@ -6,8 +6,10 @@ import com.github.gopalakrrish.springstore.api.dtos.ErrorDto;
 import com.github.gopalakrrish.springstore.api.exceptions.CartEmptyException;
 import com.github.gopalakrrish.springstore.api.exceptions.CartNotFoundException;
 import com.github.gopalakrrish.springstore.api.services.CheckoutService;
+import com.stripe.exception.StripeException;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +20,13 @@ public class CheckoutController {
     private final CheckoutService checkoutService;
 
     @PostMapping
-    public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
-        return checkoutService.checkout(request);
+    public ResponseEntity<?> checkout(@Valid @RequestBody CheckoutRequest request) {
+        try {
+            return ResponseEntity.ok(checkoutService.checkout(request));
+        } catch (StripeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
+                    body(new ErrorDto("Error creating a checkout session"));
+        }
     }
 
     @ExceptionHandler({CartNotFoundException.class, CartEmptyException.class})
--- src/main/java/com/github/gopalakrrish/springstore/api/services/CheckoutService.java
+++ src/main/java/com/github/gopalakrrish/springstore/api/services/CheckoutService.java
@@ -2,7 +2,6 @@ package com.github.gopalakrrish.springstore.api.services;
 
 import com.github.gopalakrrish.springstore.api.dtos.CheckoutRequest;
 import com.github.gopalakrrish.springstore.api.dtos.CheckoutResponse;
-import com.github.gopalakrrish.springstore.api.dtos.ErrorDto;
 import com.github.gopalakrrish.springstore.api.entities.Order;
 import com.github.gopalakrrish.springstore.api.exceptions.CartEmptyException;
 import com.github.gopalakrrish.springstore.api.exceptions.CartNotFoundException;
@@ -12,11 +11,14 @@ import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@AllArgsConstructor
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
 @Service
 public class CheckoutService {
     private final CartRepository cartRepository;
@@ -27,6 +29,7 @@ public class CheckoutService {
     @Value("${websiteUrl}")
     private String websiteUrl;
 
+    @Transactional
     public CheckoutResponse checkout(CheckoutRequest request) throws StripeException {
         var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
         if (cart == null) {
@@ -41,33 +44,41 @@ public class CheckoutService {
 
         orderRepository.save(order);
 
-        // Create a checkout session
-        var builder = SessionCreateParams.builder()
+        try {
+            // Create a checkout session
+            var builder = SessionCreateParams.builder()
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                     .setCancelUrl(websiteUrl + "/checkout-cancel");
 
-        order.getItems().forEach(item -> {
-            var lineItem = SessionCreateParams.LineItem.builder()
-                    .setQuantity(Long.valueOf(item.getQuantity()))
-                    .setPriceData(
-                            SessionCreateParams.LineItem.PriceData.builder()
-                                    .setCurrency("usd")
-                                    .setUnitAmountDecimal(item.getUnitPrice())
-                                    .setProductData(
-                                            SessionCreateParams.LineItem.PriceData.ProductData.builder()
-                                                .setName(item.getProduct().getName())
-                                                .build())
-                                    .build())
-                    .build();
+            order.getItems().forEach(item -> {
+                var lineItem = SessionCreateParams.LineItem.builder()
+                        .setQuantity(Long.valueOf(item.getQuantity()))
+                        .setPriceData(
+                                SessionCreateParams.LineItem.PriceData.builder()
+                                        .setCurrency("usd")
+                                        .setUnitAmountDecimal(
+                                            item.getUnitPrice().
+                                                multiply(BigDecimal.valueOf(100)))
+                                        .setProductData(
+                                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                        .setName(item.getProduct().getName())
+                                                        .build())
+                                        .build())
+                        .build();
 
-            builder.addLineItem(lineItem);
-        } );
+                builder.addLineItem(lineItem);
+            } );
 
-        var session = Session.create(builder.build());
+            var session = Session.create(builder.build());
 
-        cartService.clearCart(cart.getId());
+            cartService.clearCart(cart.getId());
 
-        return new CheckoutResponse(order.getId(), session.getUrl());
+            return new CheckoutResponse(order.getId(), session.getUrl());
+        } catch (StripeException ex) {
+            System.out.println(ex.getMessage());
+            orderRepository.delete(order);
+            throw ex;
+        }
     }
 }
